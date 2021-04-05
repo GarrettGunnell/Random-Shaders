d807c14e8f8edb38d7e8eaffaa57cf9fa9c3e5d8
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-04-05 00:03:45 -0700

    Blit modulus shader on render image

--- Assets/Modulus/Modulus.cs
+++ Assets/Modulus/Modulus.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Modulus : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Modulus : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Shader modulusShader;
+    private Material modulusMaterial;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        if (modulusMaterial == null) {
+            modulusMaterial = new Material(modulusShader);
+            modulusMaterial.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        Graphics.Blit(source, destination, modulusMaterial);
     }
 }
